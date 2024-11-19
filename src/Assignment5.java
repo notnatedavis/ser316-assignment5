dccb26952cb6c81b1aabe8985d2b023e587d06a0
Nathaniel Davis-Perez <ndavispe@asu.edu>
2024-11-19 10:33:39 -0500

    moved Assignment5.java to src\ package

---
+++ src/Assignment5.java
@@ -0,0 +1,17 @@
+// package x; // might need later
+
+// import x; // might need later
+package src;
+
+/**
+  * Assignment5.java
+  * @author Nathaniel Davis-Perez [ndavispe]
+  * @since 11/19/2024
+  */
+public class Assignment5 {
+  // variables here
+
+  // methods here
+
+  System.out.println("Hello World");
+}
